7a7b71676f045027b848185642d1989d6011ea64
yaronsivan <99491978+yaronsivan@users.noreply.github.com>
2025-08-15 01:32:46 +0300

    Fix tailwin PostCSS config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+// postcss.config.mjs
+export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
-
-export default config
